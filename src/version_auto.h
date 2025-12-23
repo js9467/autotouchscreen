d9f1e03d94c4c02131aa9615f0a40e504591c019
js9467 <slaughterbeck@gmail.com>
2025-12-22 21:03:46 -0500

    Trigger workflow with fixed permissions

--- src/version_auto.h
+++ src/version_auto.h
@@ -1,3 +1,3 @@
 #pragma once
-// Version 1.2.7 - Testing GitHub Actions workflow
+// Version 1.2.7 - Testing automated deployment with proper permissions
 constexpr const char* APP_VERSION = "1.2.7";
