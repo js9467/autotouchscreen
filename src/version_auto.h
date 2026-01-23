4cba1ff18100750815f5cbac1b707d1ee71500d6
js9467 <slaughterbeck@gmail.com>
2026-01-23 18:07:48 -0500

    e

--- src/version_auto.h
+++ src/version_auto.h
@@ -1,3 +1,3 @@
 #pragma once
-// Auto-generated on 2026-01-23T22:45:36.765416Z
-constexpr const char* APP_VERSION = "1.3.46";
+// Auto-generated on 2026-01-23T23:03:12.302026Z
+constexpr const char* APP_VERSION = "1.3.49";
