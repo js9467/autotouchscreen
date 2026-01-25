cead61f7b42c177358b48f3b58f97800c0cb6cb2
js9467 <slaughterbeck@gmail.com>
2026-01-25 10:37:39 -0500

    Release 1.3.56: Fix 413 errors with 2MB request limit

--- src/version_auto.h
+++ src/version_auto.h
@@ -1,3 +1,3 @@
 #pragma once
-// Auto-generated on 2026-01-25T15:31:31.591292Z
-constexpr const char* APP_VERSION = "1.3.55";
+// Auto-generated on 2026-01-25T15:36:59.103486Z
+constexpr const char* APP_VERSION = "1.3.56";
