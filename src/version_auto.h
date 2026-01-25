5cc1d9224663e6c08573b771435f480ca3f38730
js9467 <slaughterbeck@gmail.com>
2026-01-25 09:57:25 -0500

    Update 1.3.56 to use 1.3.55 firmware (with OTA validation fix)

--- src/version_auto.h
+++ src/version_auto.h
@@ -1,3 +1,3 @@
 #pragma once
-// Auto-generated on 2026-01-25T14:44:40.948210Z
-constexpr const char* APP_VERSION = "1.3.50";
+// Auto-generated on 2026-01-25T14:53:09.151946Z
+constexpr const char* APP_VERSION = "1.3.55";
